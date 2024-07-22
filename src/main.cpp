fcfd046a4e90afe1d7e3a0c4b14a3fffb00c1d24
0lbap <pablo.laviron@etu.umontpellier.fr>
2024-07-22 11:15:03 +0200

    Added debug flag

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,22 @@
 #include "models/test.cpp"
 
-int main(int, char **) {
-  std::cout << "Welcome to CCNN" << std::endl;
+int main(int argc, char *argv[]) {
+  bool debug = false;
 
-  test_model();
+  for (int i = 1; i < argc; i++) {
+    if (strcmp(argv[i], "--debug") == 0 || strcmp(argv[i], "-d") == 0) {
+      debug = true;
+      break;
+    }
+  }
 
-  std::cout << "Done." << std::endl;
+  if (debug) {
+    std::cout << "Welcome to CCNN!" << std::endl;
+    run_model_debug();
+    std::cout << "Done." << std::endl;
+  } else {
+    run_model();
+  }
 
   return EXIT_SUCCESS;
 }
--- src/models/test.cpp
+++ src/models/test.cpp
@@ -2,7 +2,123 @@
 #include <iostream>
 #include "../tensor_utils.hpp"
 
-void test_model() {
+void run_model() {
+  // Set up input tensor
+  int t1_chans = 1;
+  int t1_rows = 8;
+  int t1_cols = 8;
+  Tensor3D t1(t1_chans, Tensor2D(t1_rows, Tensor1D(t1_cols, 0)));
+
+  float cpt = 0;
+  for (int i = 0; i < t1_rows; i++) {
+    for (int j = 0; j < t1_cols; j++) {
+      t1[0][i][j] = cpt;
+      cpt++;
+    }
+  }
+
+  // Set up 2 filters of size 3x3x1 (for the first convolution)
+  int n_f1 = 2;
+  int f1_chans = 1;
+  int f1_rows = 3;
+  int f1_cols = 3;
+  std::vector<Tensor3D> f1(n_f1, Tensor3D(f1_chans, Tensor2D(f1_rows, Tensor1D(f1_cols, 0))));
+  f1 = {
+    {
+      {
+        {1, 0, 1},
+        {0, 1, 0},
+        {1, 0, 1}
+      }
+    }, {
+      {
+        {1, 1, 1},
+        {1, 1, 1},
+        {1, 1, 1}
+      }
+    }
+  };
+
+  // Conv2D
+  Tensor3D t2 = tensor_conv_3d(t1, f1, ACTIVATION_FUNCTION_RELU); // apply convolution
+
+  // AveragePooling2D
+  Tensor3D t3 = tensor_avg_pooling_3d(t2);
+
+  // Transpose
+  Tensor2D tt1(3, Tensor1D(2, 0));
+  cpt = 1;
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 2; j++) {
+      tt1[i][j] = cpt;
+      cpt++;
+    }
+  }
+  Tensor2D tt2 = tensor_transpose_2d(tt1);
+
+  Tensor2D tt3(3, Tensor1D(2, 0));
+  cpt = 1;
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 2; j++) {
+      tt3[i][j] = cpt;
+      cpt++;
+    }
+  }
+  Tensor2D tt4(2, Tensor1D(3, 0));
+  cpt = 1;
+  for (int i = 0; i < 2; i++) {
+    for (int j = 0; j < 3; j++) {
+      tt4[i][j] = cpt;
+      cpt++;
+    }
+  }
+  Tensor2D tt5 = tensor_matmul_2d(tt3, tt4);
+
+  Tensor2D tt6(1, Tensor1D(3, 0));
+  cpt = 1;
+  for (int i = 0; i < 3; i++) {
+    tt6[0][i] = cpt;
+    cpt++;
+  }
+  Tensor2D tt7(3, Tensor1D(3, 0));
+  cpt = 1;
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 3; j++) {
+      tt7[i][j] = cpt;
+      cpt++;
+    }
+  }
+  Tensor2D tt8 = tensor_matmul_2d(tt6, tt7);
+
+  Tensor2D tt9(3, Tensor1D(3, 0));
+  cpt = 1;
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 3; j++) {
+      tt9[i][j] = cpt;
+      cpt++;
+    }
+  }
+  Tensor2D tt10(3, Tensor1D(1, 0));
+  cpt = 1;
+  for (int i = 0; i < 3; i++) {
+    tt10[i][0] = cpt;
+    cpt++;
+  }
+  Tensor2D tt11 = tensor_matmul_2d(tt9, tt10);
+  
+  Tensor1D input1(3, 0);
+  input1 = {1.0, 2.0, 3.0};
+  Tensor1D bias1(2, 0);
+  bias1 = {0.0, 0.0};
+  Tensor2D weights1(2, Tensor1D(3, 0));
+  weights1 = {
+    {0.1, 0.1, 0.1},
+    {0.1, 0.1, 0.1}
+  };
+  Tensor1D output1 = tensor_dense_1d(input1, weights1, bias1, ACTIVATION_FUNCTION_SOFTMAX);
+}
+
+void run_model_debug() {
   // Set up input tensor
   int t1_chans = 1;
   int t1_rows = 8;
